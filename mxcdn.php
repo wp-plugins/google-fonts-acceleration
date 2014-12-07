bcc9f4332ccf761ae56f844a79a279103655be2d
cqdaidong <cqdaidong@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-07 17:05:25 +0000

    shengjichajian
    git-svn-id: https://plugins.svn.wordpress.org/google-fonts-acceleration/trunk@1039889 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- mxcdn.php
+++ mxcdn.php
@@ -1,21 +1,31 @@
 <?php
 /*
-	Plugin Name:Google字体加速
-	Plugin URI: http://www.fulihost.com
-	Description:本插件将自动把您网站所有Google公共库和字体库链接替换为梦想天空CDN提供的前端公共库&字体库链接。（本插件主要适用中国大陆等Google被封锁国家）如果需要替换为360公共库的请到我博客下载360公共库版本！
-	Version: 1.5
+	Plugin Name:谷歌字体与Gravatar头像加速
+	Plugin URI: http://www.iGeekLab.com
+	Description:本插件可针对面对中国大陆地区用户的网站调用的Google公共库服务和Gravatar头像服务进行替换加速。（本插件由iGeekLab.com开发维护,加速服务器端由梦想互联(屌鸡)提供技术支持,如需使用360网站卫士加速版本请到iGeekLAB.com下载。）
+	Version: 1.6
 	Author: cqdaidong
-	Author URI: http://www.fulihost.com
+	Author URI: http://www.iGeekLab.com
 */
-function spc_cdn_callback($buffer) {
+
+/*Google公共库加速*/
+function igeeklab_cdn_callback($buffer) {
 	return str_replace('googleapis.com', 'ifdream.net', $buffer);
 }
-function spc_buffer_start() {
-	ob_start("spc_cdn_callback");
+function igeeklab_buffer_start() {
+	ob_start("igeeklab_cdn_callback");
 }
-function spc_buffer_end() {
+function igeeklab_buffer_end() {
 	ob_end_flush();
 }
-add_action('init', 'spc_buffer_start');
-add_action('shutdown', 'spc_buffer_end');
+add_action('init', 'igeeklab_buffer_start');
+add_action('shutdown', 'igeeklab_buffer_end');
+
+/*gravatar头像加速*/
+function igeekLab_get_cdn_avatar($avatar) {
+	$avatar = str_replace(array( "0.gravatar.com", "1.gravatar.com",), "gravatar0.ifdream.net", $avatar);
+	$avatar = str_replace(array("www.gravatar.com", "2.gravatar.com","secure.gravatar.com"), "gravatar1.ifdream.net", $avatar);
+	return $avatar;
+}
+add_filter('get_avatar', 'igeekLab_get_cdn_avatar');
 ?>
--- readme.txt
+++ readme.txt
@@ -1,48 +1,66 @@
-﻿=== Google字体加速===
+﻿=== 谷歌字体与Gravatar头像加速===
+
 Contributors:cqdaidong
-Donate link: http://www.fulihost.com/
-Tags:Google,CDN,字体,公共库,字体库
+
+Donate link: http://www.iGeekLab.com/
+
+Tags:Google,CDN,加速,字体,Gravatar,公共库,字体库
+
 Requires at least:1.0
+
 Tested up to: 1.5
+
 Stable tag: 1.0
+
 License: GPLv2 or later
-License URI: http://www.gnu.org/licenses/gpl-2.0.html
+License URI:http://www.gnu.org/licenses/gpl-2.0.html
 
-本插件将自动把您网站所有Google公共库和字体库链接替换为梦想天空CDN提供的前端公共库&字体库链接。（本插件主要适用中国大陆等Google被封锁国家）如果需要替换为360公共库的请到我博客下载360公共库版本！
+
+本插件可针对面对中国大陆地区的网站调用的Google公共库服务和Gravatar头像服务进行替换加速。
+
 == Description ==
-本插件将自动把您网站所有Google公共库和字体库链接替换为梦想天空CDN提供的前端公共库&字体库链接。（本插件主要适用中国大陆等Google被封锁国家）如果需要替换为360公共库的请到我博客下载360公共库版本！
 
-为什么我们比360更快？
-360每次都会向Google请求公共库资源,相当于代理访问google.
-（流程：网站请求→360代理访问→Google公共库）
+本插件可针对面对中国大陆地区的网站调用的Google公共库服务和Gravatar头像服务进行替换加速。
 
-我们直接把Google公共库扒下来放在CDN集群里,无需每次都向google请求公共库.
-（流程：网站请求→Google公共库）
 
- Google公共库更新了怎么办？
-我们每三天自动更新一次Google公共库和字体库资源。
-
-鸣谢:梦想天空CDN公测项目
 
 == Installation ==
+
 
 上传插件到/wp-content/plugins/目录
 在插件列表中启用它
 
+
+
 == Frequently Asked Questions ==
 
-=加速失败？=
+=
+加速失败？=
 请确认是否开启本插件。
 
+
+
 == Screenshots ==
+
 没有
+
+
 
 == Changelog ==
 
-= 1.5 =
-* 精简代码,支持包括插件在内的全站Google公共库/字体库替换。
+= 
+1.5 =
+* 精简代码,支持包括插件在内的全站Google公共库/字体库替换
+
+
+1.6 =
+* 新增对Gravatar头像的加速
 
 == Upgrade Notice ==
 
-= 1.5=
+= 
+1.5=
 * 更换了全新的前端库地址。
+
+1.6 =
+* 新增对Gravatar头像的加速
