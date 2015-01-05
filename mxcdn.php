0b98d724490eea7d03f56147f40e4aa2343ca616
cqdaidong <cqdaidong@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-05 06:04:45 +0000

    shengji
    git-svn-id: https://plugins.svn.wordpress.org/google-fonts-acceleration/trunk@1060083 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- mxcdn.php
+++ mxcdn.php
@@ -2,15 +2,15 @@
 /*
 	Plugin Name:谷歌字体与Gravatar头像加速
 	Plugin URI: http://www.iGeekLab.com
-	Description:本插件可针对面对中国大陆地区用户的网站调用的Google公共库服务和Gravatar头像服务进行替换加速。（本插件由iGeekLab.com开发维护,加速服务器端由梦想互联(屌鸡)提供技术支持,如需使用360网站卫士加速版本请到iGeekLAB.com下载。）
-	Version: 1.6
+	Description:本插件可针对面对中国大陆地区用户的网站调用的Google公共库服务和Gravatar头像服务进行替换加速,此次更新更换了我们的全新加速源地址，且支持SSL加密调用加速源。（本插件由iGeekLab.com开发维护,加速服务器端由梦想互联 屌鸡 提供技术支持）
+	Version: 1.7
 	Author: cqdaidong
 	Author URI: http://www.iGeekLab.com
 */
 
 /*Google公共库加速*/
 function igeeklab_cdn_callback($buffer) {
-	$buffer = str_replace(array( "googleapis.com"), "ifdream.net", $buffer);
+	$buffer = str_replace(array( "googleapis.com"), "233.wiki", $buffer);
         return $buffer;
 }
 function igeeklab_buffer_start() {
@@ -24,8 +24,8 @@ add_action('shutdown', 'igeeklab_buffer_end');
 
 /*gravatar头像加速*/
 function igeekLab_cdn_avatar($avatar) {
-	$avatar = str_replace(array( "0.gravatar.com", "1.gravatar.com",), "gravatar0.ifdream.net", $avatar);
-	$avatar = str_replace(array("www.gravatar.com", "2.gravatar.com","secure.gravatar.com"), "gravatar1.ifdream.net", $avatar);
+	$avatar = str_replace(array( "0.gravatar.com", "1.gravatar.com",), "gravatar0.233.wiki", $avatar);
+	$avatar = str_replace(array("www.gravatar.com", "2.gravatar.com","secure.gravatar.com"), "gravatar1.233.wiki", $avatar);
 	return $avatar;
 }
 function igeeklab_avatar_start() {
--- readme.txt
+++ readme.txt
@@ -31,7 +31,7 @@ License URI:http://www.gnu.org/licenses/gpl-2.0.html
 == Frequently Asked Questions ==
 
 
-加速失败请先确认是否有启用其他同类插件导致互相冲突，有意见或者建议请邮件与我联系cqdaidong@gmail.com
+加速失败请先确认是否有启用其他同类插件或者代码导致互相冲突，如果您能提供更快速的加速源地址并且愿意分享可邮件联系我，我们将增加多个加速源地址切换功能。   邮箱：cqdaidong@gmail.com
 
 == Screenshots ==
 
@@ -43,8 +43,14 @@ License URI:http://www.gnu.org/licenses/gpl-2.0.html
 1.6 =
 * 由于众所周知的原因，我们在维护Google公共库加速的同时，新增了对Gravatar头像的加速。您无需再安装多个插件以及繁琐的修改主题文件，本插件占用极小,对WordPress性能影响可忽略不计。
 
+1.7 =
+* 更换了我们的全新加速源地址,并且支持SSL调用加速源，解决之前不支持SSL调用，使得Https访问网站提示风险的问题。（之前的加速源依然有效，但不支持SSL）
+
 == Upgrade Notice ==
 
 = 
 1.6 =
 * 在优化谷歌公共库加速的同时,新增对Gravatar头像的加速!（暂不支持SSL调用）
+
+1.7 =
+* 更换了我们的全新加速源地址,并且支持SSL调用加速源，解决之前不支持SSL调用，使得Https访问网站提示风险的问题。（之前的加速源依然有效，但不支持SSL）
